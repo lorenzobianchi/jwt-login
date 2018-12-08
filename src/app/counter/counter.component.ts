bdd931e61fe85eac6e5c293c5a57a0b14bfb2976
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2018-12-08 18:56:52 +0100

    first commit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Increment, Decrement, Reset } from './counter.actions';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent implements OnInit {
+  count$: Observable<number>;
+  constructor(private store: Store<{ count: number}>) {
+    this.count$ = store.pipe(select('count'))
+  }
+
+  ngOnInit() {
+  }
+
+  increment() {
+    this.store.dispatch(new Increment());
+  }
+
+  decrement() {
+    this.store.dispatch(new Decrement());
+  }
+
+  reset() {
+    this.store.dispatch(new Reset());
+  }
+}
